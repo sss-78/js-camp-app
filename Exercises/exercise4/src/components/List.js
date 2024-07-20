3cf2cfa18655977bf83f77a42aa40f37dac118b9
Sai Sreeramani <saisuhas2004@gmail.com>
2024-07-20 17:10:43 -0400

    Added E4 code

--- Exercises/exercise4/src/components/List.js
+++ Exercises/exercise4/src/components/List.js
@@ -1,4 +1,12 @@
 
-export default function List() {
-    
+export default function List({display}) {
+    return (
+        <>
+            <ul>
+                {display.map((match, idx) => (
+                    <li key={idx}>{match.city} - {match.state} - {match.population}</li>
+                ))}
+            </ul>
+        </>
+    )
 }
